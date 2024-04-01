adf84131915eef284b1507a08152a72482bf582c
loverikim <loverikim03@gmail.com>
2024-04-02 01:58:43 +0900

    지역에 따른 날씨 정보 안내 프로그램 만들기 오류가 있었던 어제보다 높은지 낮은지 while 문을 활용한 반복 업데이트

---
+++ pyTest.py
@@ -0,0 +1,8 @@
+# 파이썬 패키지에서 pyinstaller 설치 최신버전보단 구버전에 나음
+# 실행 파일로 바꿔주는 역할
+# terminal 창으로 이동
+# 옵션 사항이 많음.
+# 텍스트만 들어간 파일은 원파일로 --> pyinstaller --onefile googleTrans20240401.py
+# 이미지도 있고 파일도 첨부된 것은 이걸로 배포 -->
+# pyinstaller --windowed 파일이름.py
+# dist 폴더안에 생성.
